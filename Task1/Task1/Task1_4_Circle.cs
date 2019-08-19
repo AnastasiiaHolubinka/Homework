d211b27b8e96c172b5543ecf52ae3b01a5ffeb0e
“Nazar <nazarpelypets@ukr.net>
2019-08-19 20:25:32 +0300

    Task1 and Task2 add

---
+++ Task1/Task1/Task1_4_Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    static class Task1_4_Circle
+    {
+        const double pi = 3.14159;
+
+       public static double S(double r)
+        {
+            return pi * r * r;
+        }
+       public static double C(double r)
+        {
+            return 2 * pi * r;
+        }
+    }
+}
